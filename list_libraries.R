9e8dce1682b6ae41b1569609e6fe152876f9d192
hudracyador <77686460+hudracyador@users.noreply.github.com>
2023-11-18 14:38:36 +0100

    Created "list_libraries.R" to handle showing loading and unloading libraries/packages in RStudio.

---
+++ list_libraries.R
@@ -0,0 +1,12 @@
+# The function library() will show every package and its version installed in our computer
+# However it is too much information
+# We can create a vector with the name of the packages to check later if a package is in it
+
+my_packages <- library()$results[,1]
+head(my_packages, 10)
+
+# We can list the libraries that are actually loaded doing
+(.packages())
+
+# We can unload packages that are no longe needed
+unloadNamespace("tidyr")
