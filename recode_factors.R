02b641602578f4ac0a2a703bfdf1dcb43304e971
hudracyador <77686460+hudracyador@users.noreply.github.com>
2023-12-18 15:22:53 +0100

    Added code for recoding all scales from <num> to <fct> to facilitate regression modelling

--- recode_factors.R
+++ recode_factors.R
@@ -1,22 +1,63 @@
-# Create levels for SRL and SE scales
-levels_srl_se <- c(
-  "trifft überhaupt nicht auf mich zu",
-  "trifft manchmal auf mich zu",
-  "trifft eher auf mich zu",
-  "trifft auf mich zu",
-  "trifft sehr auf mich zu",
-)
+library(dplyr)
 
-# Create levels for WLA and WLC scales
-levels_wla_wlc <- c(
-  "nie",
-  "ein- oder zweimal",
-  "manchmal",
-  "oft",
-  "sehr oft oder ständig"
-)
+# Create subsets from all scales for converting from <num> to <fct>
 
-wla_factors <- as.data.frame(factor(df_srl[,8:19], levels = levels_wla_wlc))
-View(wla_factors)
+subset_wla_fct <- df_srl_imp_mean |> select(c(starts_with("WLA_"),))
+subset_srl_f_fct <- df_srl_imp_mean |> select(c(starts_with("SRL_F_"),))
+subset_srl_p_fct <- df_srl_imp_mean |> select(c(starts_with("SRL_P_"),))
+subset_srl_sr_fct <- df_srl_imp_mean |> select(c(starts_with("SRL_SR_"),))
+subset_wlc_fct <- df_srl_imp_mean |> select(c(starts_with("WLC_"),))
+subset_se_fct <- df_srl_imp_mean |> select(c(starts_with("SE_"),))
+
+# Convert WLA subset from <num> to <fct>
+subset_wla_fct <- subset_wla_fct |>
+  mutate(
+    across(where(is.numeric), as.factor)
+  )
+# Check if WLA subset is <fct> across all variables
+sapply(subset_wla_fct, class)
 
 
+# Convert SRL-F subset from <num> to <fct>
+subset_srl_f_fct <- subset_srl_f_fct |>
+  mutate(
+    across(where(is.numeric), as.factor)
+  )
+# Check if SRL-F subset is <fct> across all variables
+sapply(subset_srl_f_fct, class)
+
+
+# Convert SRL-P subset from <num> to <fct>
+subset_srl_p_fct <- subset_srl_p_fct |>
+  mutate(
+    across(where(is.numeric), as.factor)
+  )
+# Check if SRL-P subset is <fct> across all variables
+sapply(subset_srl_p_fct, class)
+
+
+# Convert SRL-SR subset from <num> to <fct>
+subset_srl_sr_fct <- subset_srl_sr_fct |>
+  mutate(
+    across(where(is.numeric), as.factor)
+  )
+# Check if SRL-SR subset is <fct> across all variables
+sapply(subset_srl_sr_fct, class)
+
+
+# Convert WLC subset from <num> to <fct>
+subset_wlc_fct <- subset_wlc_fct |>
+  mutate(
+    across(where(is.numeric), as.factor)
+  )
+# Check if WLC subset is <fct> across all variables
+sapply(subset_wlc_fct, class)
+
+
+# Convert SE subset from <num> to <fct>
+subset_se_fct <- subset_se_fct |>
+  mutate(
+    across(where(is.numeric), as.factor)
+  )
+# Check if SE subset is <fct> across all variables
+sapply(subset_se_fct, class)
