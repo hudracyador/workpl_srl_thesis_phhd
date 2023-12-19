d0f4e489d3da4d6b4af773f69334a929c64fad9b
hudracyador <77686460+hudracyador@users.noreply.github.com>
2023-12-19 17:22:20 +0100

    Added code for calculating Sobel's test

--- regression_mediation.R
+++ regression_mediation.R
@@ -1,3 +1,6 @@
+library(psych)
+library(multilevel)
+
 # Calculate single linear regression regression model for
 # hypothesis H1a
 # Linear regression analysis (Enter method)  
@@ -40,3 +43,95 @@ lm_h1c <- lm(
     srl_sr_f2,
   data = factors_regression
 )
+
+# Calculate Sobel's test values for hypothesis H1d
+library(bda)
+
+# srl_f_f1
+med_srl_f_f1 <- mediation.test(
+factors_regression$wla_f1,
+factors_regression$srl_f_f1,
+factors_regression$wlc_f1
+)
+med_srl_f_f1
+
+# srl_f_f2
+med_srl_f_f2 <- mediation.test(
+  factors_regression$wla_f1,
+  factors_regression$srl_f_f1,
+  factors_regression$wlc_f1
+)
+med_srl_f_f2
+
+# srl_f_f3
+med_srl_f_f3 <- mediation.test(
+  factors_regression$wla_f1,
+  factors_regression$srl_f_f1,
+  factors_regression$wlc_f1
+)
+med_srl_f_f3
+
+# srl_f_f4
+med_srl_f_f4 <- mediation.test(
+  factors_regression$wla_f1,
+  factors_regression$srl_f_f4,
+  factors_regression$wlc_f1
+)
+med_srl_f_f4
+
+# srl_p_f1
+med_srl_p_f1 <- mediation.test(
+  factors_regression$wla_f1,
+  factors_regression$srl_p_f1,
+  factors_regression$wlc_f1
+)
+med_srl_p_f1
+
+# srl_p_f2
+med_srl_p_f2 <- mediation.test(
+  factors_regression$wla_f1,
+  factors_regression$srl_p_f2,
+  factors_regression$wlc_f1
+)
+med_srl_p_f2
+
+# srl_p_f3
+med_srl_p_f3 <- mediation.test(
+  factors_regression$wla_f1,
+  factors_regression$srl_p_f3,
+  factors_regression$wlc_f1
+)
+med_srl_p_f3
+
+
+# srl_sr_f1
+med_srl_sr_f1 <- mediation.test(
+  factors_regression$wla_f1,
+  factors_regression$srl_sr_f1,
+  factors_regression$wlc_f1
+)
+med_srl_sr_f1
+
+# srl_sr_f2
+med_srl_sr_f2 <- mediation.test(
+  factors_regression$wla_f1,
+  factors_regression$srl_sr_f2,
+  factors_regression$wlc_f1
+)
+med_srl_sr_f2
+
+# sobel_comparison <- as.data.frame(
+#   c(
+#     med_srl_f_f1$Sobel,
+#     med_srl_f_f2$Sobel,
+#     med_srl_f_f3$Sobel,
+#     med_srl_f_f4$Sobel,
+#     med_srl_p_f1$Sobel,
+#     med_srl_p_f2$Sobel,
+#     med_srl_p_f3$Sobel,
+#     med_srl_sr_f1$Sobel,
+#     med_srl_sr_f2$Sobel
+#   )
+# )
+# 
+# View(sobel_comparison)
