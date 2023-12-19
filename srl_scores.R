2318bc6195180bd47b7edd58ad7aef75763e7bcb
hudracyador <77686460+hudracyador@users.noreply.github.com>
2023-12-19 17:22:44 +0100

    Contents of this script have been moved to other files

--- srl_scores.R
+++ srl_scores.R
@@ -1,2 +1,16 @@
-srlScore <- rowSums(subset_srl)
-View(srlScore)
+library(dplyr)
+
+# Get SRL scores by calculating row sums of individual observations
+srl_scores <- as.data.frame(rowSums(subset_srl))
+colnames(srl_scores)[1] <- "srl_scores"
+View(srl_scores)
+
+factors_regression <- factors_regression |>
+  mutate(
+    new_col = srl_scores$srl_scores
+  )
+colnames(factors_regression)[13] <- "srl_scores"
+View(factors_regression)
+
+# Calculate Pearson´s correlation between all factors and SRL score
+conv_valid <- cor(factors_regression, method = "pearson")
