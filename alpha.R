b3af4dcdf508c9c426fbd7935ba135d99866aeec
hudracyador <77686460+hudracyador@users.noreply.github.com>
2024-01-23 00:12:48 +0100

    Added unload for "scales" package, changed alpha values

--- alpha.R
+++ alpha.R
@@ -1,23 +1,25 @@
 library(psych)
 unloadNamespace("ggplot2")
+unloadNamespace("scales")
 
 # Calculate the reliability value from the respective subset
 # check.key=TRUE checks for possible inverse coding
 
-# raw_alpha: 0.81
+# raw_alpha: 0.77
 alpha(subset_wla, check.keys = TRUE)
 
-# raw_alpha: 0.81
+# raw_alpha: 0.84
 alpha(subset_srl_f, check.keys = TRUE)
 
-# raw_alpha: 0.86
+# raw_alpha: 0.84
 alpha(subset_srl_p, check.keys = TRUE)
 
-# raw_alpha: 0.83
+# raw_alpha: 0.82
 alpha(subset_srl_sr, check.keys = TRUE)
 
-# raw_alpha: 0.7
+# raw_alpha: 0.68
 alpha(subset_wlc, check.keys = TRUE)
 
-# raw_alpha: 0.88
+# raw_alpha: 0.87
 alpha(subset_se, check.keys = TRUE)
+
